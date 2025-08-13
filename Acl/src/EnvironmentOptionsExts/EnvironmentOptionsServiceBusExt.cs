c92539c0c4445e28c811fd780f176d34882c91e7
Xavier <xavierjohn@hotmail.com>
2025-08-13 16:53:04 -0700

    Service bus

--- Acl/src/EnvironmentOptionsExts/EnvironmentOptionsServiceBusExt.cs
+++ Acl/src/EnvironmentOptionsExts/EnvironmentOptionsServiceBusExt.cs
@@ -4,13 +4,13 @@ public static class EnvironmentOptionsServiceBusExt
 {
     public static string GetServiceBusName(this EnvironmentOptions settings) => settings.GetResourceNameShared("sbns");
 
-    public static string GetServiceBusNamespace(this EnvironmentOptions settings)
-    {
-        if (settings.Cloud == CloudType.AzureCloud)
-            return $"{GetServiceBusName(settings)}.servicebus.windows.net";
-        if (settings.Cloud == CloudType.AzureUSGovernment)
-            return $"{GetServiceBusName(settings)}.servicebus.usgovcloudapi.net";
-
-        throw new NotSupportedException($"Cloud type '{settings.Cloud}' is not supported.");
-    }
+    public static string GetServiceBusNamespace(this EnvironmentOptions settings) =>
+        settings.Cloud switch
+        {
+            CloudType.AzureCloud => $"{settings.GetServiceBusName()}.servicebus.windows.net",
+            CloudType.AzureUSGovernment => $"{settings.GetServiceBusName()}.servicebus.usgovcloudapi.net",
+            CloudType.AzureChinaCloud => $"{settings.GetServiceBusName()}.servicebus.chinacloudapi.cn",
+            CloudType.AzureGermanCloud => $"{settings.GetServiceBusName()}.servicebus.cloudapi.de",
+            _ => throw new NotSupportedException($"Cloud type '{settings.Cloud}' is not supported.")
+        };
 }
