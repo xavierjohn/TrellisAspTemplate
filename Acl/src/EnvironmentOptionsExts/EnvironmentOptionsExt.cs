1b5b38178b1173f730973552dd8e85a4d1cfa815
Xavier <xavierjohn@hotmail.com>
2025-08-14 09:35:35 -0700

    Add comment

--- Acl/src/EnvironmentOptionsExts/EnvironmentOptionsExt.cs
+++ Acl/src/EnvironmentOptionsExts/EnvironmentOptionsExt.cs
@@ -2,9 +2,13 @@
 
 public static class EnvironmentOptionsExt
 {
+    // Region specific resources.
+    // Example: App Service, Key Vault, Managed Identity. etc.
     public static string GetResourceName(this EnvironmentOptions settings, string resourceType) =>
         $"{settings.Environment}-{settings.RegionShortName}-{settings.ServiceName}-{resourceType}".ToLowerInvariant();
 
+    // Shared resources.
+    // Example: Storage Account, Cosmos DB, SQL etc.
     public static string GetResourceNameShared(this EnvironmentOptions settings, string resourceType) =>
         $"{settings.Environment}-{settings.ServiceName}-{resourceType}".ToLowerInvariant();
 }
